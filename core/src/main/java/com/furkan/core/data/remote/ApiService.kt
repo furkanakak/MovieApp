8807b425e12ef03c63ccdc2a7d1409473d2b9487
furkan akalın <furkanakalin66ff@gmail.com>
2024-04-28 14:52:29 +0300

    Update return type of getMovies in ApiService

--- core/src/main/java/com/furkan/core/data/remote/ApiService.kt
+++ core/src/main/java/com/furkan/core/data/remote/ApiService.kt
@@ -1,13 +1,14 @@
-package com.furkan.core.data.remote.apiservice
+package com.furkan.core.data.remote
 
+import com.furkan.core.data.entity.MovieResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("movie/popular")
-    fun getMovies(
+   suspend fun getMovies(
         @Query("api_key") apiKey: String,
         @Query("page") page: Int
-    ): Response<String>
+    ): Response<MovieResponse>
 }
