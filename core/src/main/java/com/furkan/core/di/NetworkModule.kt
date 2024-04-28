3c41c66050797ca67191874bd988cc34d9a6788f
furkan akalın <furkanakalin66ff@gmail.com>
2024-04-28 12:04:32 +0300

    Add network module for handling API requests

---
+++ core/src/main/java/com/furkan/core/di/NetworkModule.kt
@@ -0,0 +1,46 @@
+package com.furkan.core.di
+
+import com.furkan.core.common.Constants
+import com.furkan.core.common.ServiceLocator
+import com.furkan.core.data.remote.apiservice.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+class NetworkModule {
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val logger = HttpLoggingInterceptor()
+        return logger
+    }
+
+    @Provides
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
