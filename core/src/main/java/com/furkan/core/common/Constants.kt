770b6b14382040fa40e6552d667bae8483d879e3
furkan akalın <furkanakalin66ff@gmail.com>
2024-04-28 23:39:41 +0300

    Update Constants class with new global values

--- core/src/main/java/com/furkan/core/common/Constants.kt
+++ core/src/main/java/com/furkan/core/common/Constants.kt
@@ -2,5 +2,7 @@ package com.furkan.core.common
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/"
+    const val IMAGE_SIZE = "w500"
     const val API_KEY = "f205d3572453d7b8994b0a534502e30b"
 }
