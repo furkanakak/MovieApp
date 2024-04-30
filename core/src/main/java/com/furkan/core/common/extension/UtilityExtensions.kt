52a837ee34e22669090cc8b88bafc5189277f81b
furkan akalın <furkanakalin66ff@gmail.com>
2024-05-01 01:58:16 +0300

    Add extension function to check internet availability

--- core/src/main/java/com/furkan/core/common/extension/UtilityExtensions.kt
+++ core/src/main/java/com/furkan/core/common/extension/UtilityExtensions.kt
@@ -1,6 +1,8 @@
 package com.furkan.core.common.extension
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.util.Log
 import android.widget.Toast
 import com.furkan.core.common.Constants
@@ -44,6 +46,24 @@ infix fun String.log(message: String) {
     if (ServiceLocator.getAppConfig().isDebug) {
         Log.d(this, message)
     }
+}
+
+
+/**
+ * Checks if there is an active internet connection available on the device.
+ * This method utilizes the ConnectivityManager to check the network status and
+ * its capabilities to determine internet accessibility.
+ *
+ * @return Returns true if an active network with internet capabilities (Wi-Fi, Cellular, Ethernet) is available.
+ * Returns false if there is no active network or the active network does not have internet capabilities.
+ */
+fun Context.isInternetAvailable(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = connectivityManager.activeNetwork ?: return false
+    val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+    return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
 }
 
 
